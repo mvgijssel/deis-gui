3188b9e238860a9faf4da0a24e607ae78886dbcb
Maarten van Gijssel <maarten@vgijssel.nl>
2014-09-22 21:12:35 +0200

    Created vendor.js with all libraries

---
+++ app/assets/javascripts/vendors.js
@@ -0,0 +1,9 @@
+// window.React = require('react');
+// require('react_ujs');
+// require('jquery');
+// require('contrib/react_glue');
+// require('app_main');
+
+window.React = require('react');
+require('react_ujs');
+require('jquery');
