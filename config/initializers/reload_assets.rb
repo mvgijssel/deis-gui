a8a956b35fb0250515e097e886f5c3fef2a0f390
Maarten van Gijssel <maarten@hackerone.com>
2014-09-23 11:56:37 +0200

    Commented reloading of assets

--- config/initializers/reload_assets.rb
+++ config/initializers/reload_assets.rb
@@ -1,13 +1,13 @@
-directories = {
-  "#{Rails.root}/app/frontend" => [:coffee],
-}
-
-watcher = Rails.application.config.file_watcher.new([], directories) do
-  p "RELOADED FRONTEND COFFEE FILES"
-end
-
-Rails.application.reloaders << watcher
-
-ActionDispatch::Reloader.to_prepare do
-  watcher.execute_if_updated
-end
+# directories = {
+#   "#{Rails.root}/app/frontend" => [:coffee],
+# }
+#
+# watcher = Rails.application.config.file_watcher.new([], directories) do
+#   p "RELOADED FRONTEND COFFEE FILES"
+# end
+#
+# Rails.application.reloaders << watcher
+#
+# ActionDispatch::Reloader.to_prepare do
+#   watcher.execute_if_updated
+# end
