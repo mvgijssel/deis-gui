a7c968b7e9e36a3645e754155fe49a8157d6ca3d
Maarten van Gijssel <maarten@hackerone.com>
2014-09-23 20:25:44 +0200

    Only use sprockets for bundled browserified files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,2 @@
-// kind of ugly to define it like this
-// so the ujs and glue stuff works
-// window.React = require('react');
-// require('react_ujs');
-// require('contrib/react_glue');
-
-window.ReactHome = require('react_home');
+//= require vendor.js
+//= require app.js
