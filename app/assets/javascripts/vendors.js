7f9d31388e47cf93bcd03f6b04c666fc41f57d53
Maarten van Gijssel <maarten@hackerone.com>
2014-09-23 11:56:18 +0200

    Removed vendors js

--- app/assets/javascripts/vendors.js
+++
@@ -1,9 +0,0 @@
-// window.React = require('react');
-// require('react_ujs');
-// require('jquery');
-// require('contrib/react_glue');
-// require('app_main');
-
-window.React = require('react');
-require('react_ujs');
-require('jquery');
