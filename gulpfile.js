677d3a55ecb1489ee08b36d7aee3e0a0a26fba10
Maarten van Gijssel <maarten@vgijssel.nl>
2014-09-30 19:29:26 +0200

    Added sass to gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
+var sass = require('gulp-sass');
+var watch = require('gulp-watch');
+var sassGraph = require('gulp-sass-graph');
+
 var browserify = require('browserify');
 var watchify = require('watchify');
 var source = require('vinyl-source-stream');
@@ -33,4 +37,43 @@ gulp.task('default', function() {
   function logBundle(message) {
 
   }
+});
+
+gulp.task('watch-sass', function() {
+  var sassLoadPaths = [
+    './app/assets/stylesheets/**/*.scss',
+    './node_modules/bootstrap-sass/assets/stylesheets'
+  ];
+  //var sassLoadPaths = './app/assets/stylesheets/home.css.scss';
+
+  // watch(sassLoadPaths)
+  //   .pipe(sassGraph([
+  //     './app/assets/stylesheets/home.css.scss',
+  //     './node_modules/bootstrap-sass/assets/stylesheets'
+  //   ]))
+  //   .pipe(sass())
+  //   .pipe(gulp.dest('./web/dist/css'));
+
+  //gulp.src(sassLoadPaths)
+  watch(sassLoadPaths)
+   .pipe(sassGraph(sassLoadPaths))
+   .pipe(sass({
+      includePaths: ['./node_modules/bootstrap-sass/assets/stylesheets'],
+      onSuccess: function() { gutil.log('Scss done') }
+   }))
+   .pipe(gulp.dest('./web/dist/css'));
+
+  // return watch(sassLoadPaths)
+  //   .pipe(gutil.log('kerk'));
+
+//  gutil.log(sassGraph(sassLoadPaths));
+
+  // return
+    //watch(sassLoadPaths
+  //, {emitOnGlob: false, name: "Sass"})
+  //   .pipe(sassGraph(sassLoadPaths))
+  //   .pipe(sass({loadPath: sassLoadPaths}))
+  //   .pipe(notify('Sass compiled <%= file.relative %>'))
+  //   .pipe(gulp.dest('web/dist/css'))
+  //   .pipe(livereload());
 });
